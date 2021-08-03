ca62a0e230fe12520aca2e6c7c738943f235b52c
Nabeshima <a_nabeshima@currentia.co.jp>
2021-08-03 11:11:26 +0900

    ファイルをts(x)化しました css moduleをやめて、styledcomponetを使うようにしました

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import "../styles/globals.css"
+import { AppProps } from "next/app"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
 
 export default MyApp
